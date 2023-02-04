7e892d146bc47a83448876c47cc55c34f2b60120
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-02-04 23:59:31 +0100

    Fix upload issue

--- api/baseapi/settings.py
+++ api/baseapi/settings.py
@@ -104,7 +104,7 @@ JWT_TOKEN_EXP_HOURS = 5 * 365 * 24  # 5 years
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = list(default_methods)
-CORS_ALLOW_HEADERS = list(default_headers) + ["Range"]
+CORS_ALLOW_HEADERS = list(default_headers) + ["Content-Disposition", "Range"]
 CORS_PREFLIGHT_MAX_AGE = 86400
 CORS_ALLOW_CREDENTIALS = False
 CORS_URLS_REGEX = r"^/v1/.*$"
