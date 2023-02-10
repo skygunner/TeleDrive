46dc4a1e43bdd0f4672d1544d72f2a757179f2f6
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-02-10 12:23:59 +0100

    Add terraform outputs

--- infra/main.tf
+++ infra/main.tf
@@ -20,14 +20,13 @@ resource "digitalocean_ssh_key" "teledrive" {
 }
 
 resource "digitalocean_droplet" "all_in_one" {
-    image              = "ubuntu-20-04-x64"
-    name               = "all-in-one.teledrive.io"
-    region             = var.digitalocean_region
-    size               = "s-1vcpu-1gb" # https://slugs.do-api.dev/
-    vpc_uuid           = digitalocean_vpc.teledrive.id
-    ssh_keys           = [ 
+    image    = "ubuntu-20-04-x64"
+    name     = "all-in-one.teledrive.io"
+    region   = var.digitalocean_region
+    size     = "s-1vcpu-1gb" # https://slugs.do-api.dev/
+    vpc_uuid = digitalocean_vpc.teledrive.id
+    ssh_keys = [ 
         digitalocean_ssh_key.teledrive.fingerprint 
     ]
-    tags               = [ "all-in-one" ]
-    private_networking = true
+    tags     = [ "all-in-one" ]
 }
---
+++ infra/outputs.tf
@@ -0,0 +1,9 @@
+output "all_in_one_ipv4_address" {
+    description = "All in One Droplet IP Address"
+    value = digitalocean_droplet.all_in_one.ipv4_address
+}
+
+output "all_in_one_ipv4_address_private" {
+    description = "All in One Droplet IP Address Private"
+    value = digitalocean_droplet.all_in_one.ipv4_address_private
+}
