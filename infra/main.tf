f10340de635f7a6f9b7f9f3d296d94b51ede69b6
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-02-10 12:09:52 +0100

    Add infra virtual machine

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,9 @@ resource "digitalocean_project" "teledrive" {
     purpose     = "Web Application"
     environment = "Production"
     is_default  = false
-    resources   = []
+    resources   = [
+        digitalocean_droplet.all_in_one.urn
+    ]
 }
 
 resource "digitalocean_vpc" "teledrive" {
@@ -15,4 +17,16 @@ resource "digitalocean_vpc" "teledrive" {
 resource "digitalocean_ssh_key" "teledrive" {
   name       = "TeleDrive"
   public_key = file("./id_rsa.pub")
+}
+
+resource "digitalocean_droplet" "all_in_one" {
+    image    = "ubuntu-20-04-x64"
+    name     = "all-in-one.teledrive.io"
+    region   = var.digitalocean_region
+    size     = "s-1vcpu-1gb" # https://slugs.do-api.dev/
+    vpc_uuid = digitalocean_vpc.teledrive.id
+    ssh_keys = [ 
+        digitalocean_ssh_key.teledrive.fingerprint 
+    ]
+    tags     = [ "all-in-one" ]
 }
