3cfe0d8b3d73f201c7cfd94cc26ff707c5e1cde7
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-02-11 19:54:36 +0100

    Add web DNS record

--- infra/main.tf
+++ infra/main.tf
@@ -79,6 +79,13 @@ resource "digitalocean_firewall" "all_in_one" {
     }
 }
 
+resource "cloudflare_record" "teledrive" {
+    zone_id = var.cloudflare_zone_id
+    name    = "@"
+    value   = digitalocean_droplet.all_in_one.ipv4_address
+    type    = "A"
+}
+
 resource "cloudflare_record" "api_teledrive" {
     zone_id = var.cloudflare_zone_id
     name    = "api"
