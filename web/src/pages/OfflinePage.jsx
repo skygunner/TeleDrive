34db5894f8925aee9e4b59887e1de9684d9978a8
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-03-17 13:46:37 +0100

    Fix offline page

--- web/src/pages/OfflinePage.jsx
+++ web/src/pages/OfflinePage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Result } from 'antd';
 import { useTranslation } from 'react-i18next';
+import { RiWifiOffLine } from 'react-icons/ri';
 
 function OfflinePage() {
   const { t } = useTranslation();
 
   return (
     <Result
-      status="500"
-      title={t('Offline')}
-      subTitle={t('You are offline!')}
+      className="center-screen"
+      icon={<RiWifiOffLine size={175} />}
+      title={t('There is no internet connection')}
+      subTitle={t('Your computer is offline')}
     />
   );
 }
