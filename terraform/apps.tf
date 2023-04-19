bc641fc0d91a6f1b18e984ce3f4656bf283301f0
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-04-19 17:29:47 +0200

    Update terraform module version

--- terraform/apps.tf
+++ terraform/apps.tf
@@ -1,12 +1,12 @@
 module "app" {
-  source = "git::https://github.com/RashadAnsari/aws-free-tier-app-terraform.git?ref=v1.0.1"
+  source = "git::https://github.com/RashadAnsari/aws-free-tier-app-terraform.git?ref=v1.0.2"
 
   app_name              = "TeleDrive"
   app_env               = "production"
   app_domain            = "teledrive.io"
   web_app_port          = 8080
   api_app_port          = 8000
-  aws_ubuntu_ami        = "ami-06d1a5507784edbad"
+  aws_ami               = "ami-06d1a5507784edbad"
   db_username           = "baseapi"
   db_password           = var.db_password
   web_server_count      = 1
